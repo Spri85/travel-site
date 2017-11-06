0384e291e2d34e13ca82527c3faa2e993d1dc311
Spri85 <viktorpar@gmail.com>
2017-11-06 09:05:54 +0200

    added css

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -3,11 +3,12 @@ var gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
   cssnested = require('postcss-nested'),
-  cssimport = require('postcss-import');
+  cssimport = require('postcss-import'),
+  cssmixins = require('postcss-mixins');
 
 gulp.task('styles', function () {
       return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))
+        .pipe(postcss([cssimport, cssmixins, cssvars, cssnested, autoprefixer]))
         .on('error', function(errorInfo) {
           console.log(errorInfo.toString());
           this.emit('end');
