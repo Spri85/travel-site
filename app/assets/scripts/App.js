df5dca5836dc9c91315ddfd8858cf39381bd7efd
Spri85 <viktorpar@gmail.com>
2017-11-09 12:37:48 +0200

    Completed the reval on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevalOnScroll from './modules/RevalOnScroll';
+import $ from 'jquery';
 
 var mobilemenunew = new MobileMenu();
+new RevalOnScroll($(".feature-item"), '85%');
+new RevalOnScroll($(".testimonial"), "70%");
