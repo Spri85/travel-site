91b70e394496a23d64c3af14988d9bb70d9a95e6
Spri85 <viktorpar@gmail.com>
2017-11-10 09:59:24 +0200

    completed Modal form

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevalOnScroll from './modules/RevalOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import $ from 'jquery';
+import Modal from './modules/Modal';
 
 var mobilemenunew = new MobileMenu();
 new RevalOnScroll($(".feature-item"), '85%');
 new RevalOnScroll($(".testimonial"), "70%");
 var stickyHeader = new StickyHeader;
+var modal = new Modal();
